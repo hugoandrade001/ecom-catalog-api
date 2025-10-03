1e7887fe1219dab4c6b7de582bed18b0803feb98
Hugo <hugoandrade@Hugos-MacBook-Air.local>
2025-10-02 21:23:18 -0300

    Remove projeto Java antigo

--- Desktop/productcatalog/src/main/java/com/ecom/productcatalog/repository/CatalogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecom.productcatalog.repository;
-
-import com.ecom.productcatalog.models.CatalogModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CatalogRepository extends JpaRepository<CatalogModel, Long> {
-}
